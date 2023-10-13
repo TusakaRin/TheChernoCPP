fd5c080f27ad197b3d741f8418c2dee889d216db
Yuanfeng Dai <yuanfeng.dai@163.sufe.edu.cn>
2023-10-13 14:42:12 +0800

    build own static library

---
+++ 51Game/51Game/src/Main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <engine.h>
+
+
+int main() {
+	engine::PrintMessage();
+	std::cin.get();
+}
---
+++ 51Game/Engine/src/Engine.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "Engine.h"
+
+
+namespace engine {
+	void PrintMessage() {
+		std::cout << "Message" << std::endl;
+	}
+}
